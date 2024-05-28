419a31ce5aab811261cbd2e8873c6c0652016edf
MathissGit <mathissineux@gmail.com>
2024-05-28 11:06:21 +0200

    add datas for test

---
+++ src/datas/users.js
@@ -0,0 +1,67 @@
+
+import FabriceEboue from '../assets/profil.jpg'
+
+export const users = [
+	{
+		id: '1',
+		lastname: 'Eboué',
+		firstname: 'Fabrice',
+		datebirth: '01/02/1990',
+		email: 'fabrice@eboue.com',
+		role: 'patient',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024'
+	},
+	{
+		id: '2',
+		lastname: 'Benbaloulie',
+		firstname: 'Nahim',
+		datebirth: '05/07/1800',
+		email: 'nahim@nahim.com',
+		role: 'Coach',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024'
+	},
+	{
+		id: '3',
+		lastname: 'Sineux',
+		firstname: 'Mathis',
+		datebirth: '20/01/2003',
+		email: 'mathis@mathis.com',
+		role: 'patient',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024'
+	},
+	{
+		id: '4',
+		lastname: 'Werquin',
+		firstname: 'Lucien',
+		datebirth: '01/02/1990',
+		email: 'lucien@lucien.com',
+		role: 'patient',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024',
+		dateRegistration: '28/05/2024'
+	},
+	{
+		id: '5',
+		lastname: 'Lamonier',
+		firstname: 'Alexis',
+		datebirth: '01/02/1990',
+		email: 'alexis@alexis.com',
+		role: 'patient',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024'
+	},
+	{
+		id: '6',
+		lastname: 'admin',
+		firstname: 'admin',
+		datebirth: '01/02/1990',
+		email: 'admin@admin.com',
+		role: 'admin',
+		avatar: FabriceEboue,
+		dateRegistration: '28/05/2024'
+	},
+
+]
