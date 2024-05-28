64c611d4068e5aa0bc52e99051609885d5f87716
lucien-wrq <luluwerq@gmail.com>
2024-05-28 10:37:14 +0200

    sidebar compact

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List, ListItem, ListItemIcon, ListItemText, Switch, FormControlLabel } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import PostAddIcon from '@mui/icons-material/PostAdd';
@@ -10,86 +10,99 @@ import GroupIcon from '@mui/icons-material/Group';
 import SettingsIcon from '@mui/icons-material/Settings';
 import BuildIcon from '@mui/icons-material/Build';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Brightness2Icon from '@mui/icons-material/Brightness2';
 import { Link } from 'react-router-dom';
 
 const Sidebar = ({ toggleDarkMode, darkMode }) => {
+    const [hover, setHover] = useState(false);
+
     return (
-        <div className="sidebar">
+        <div 
+            className={`sidebar ${hover ? 'sidebar-expanded' : 'sidebar-collapsed'}`}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
             <List className="sidebar-list">
                 <ListItem button component={Link} to="/">
                     <ListItemIcon>
                         <HomeIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Dashboard' />
+                    <ListItemText primary='Dashboard' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/post">
                     <ListItemIcon>
                         <PostAddIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Post' />
+                    <ListItemText primary='Post' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/cours">
                     <ListItemIcon>
                         <SchoolIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Cours' />
+                    <ListItemText primary='Cours' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/messages">
                     <ListItemIcon>
                         <MessageIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Messages' />
+                    <ListItemText primary='Messages' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/rendez-vous">
                     <ListItemIcon>
                         <CalendarTodayIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Rendez-vous' />
+                    <ListItemText primary='Rendez-vous' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/transaction">
                     <ListItemIcon>
                         <PaymentIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Transaction' />
+                    <ListItemText primary='Transaction' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/utilisateurs">
                     <ListItemIcon>
                         <GroupIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Utilisateurs' />
+                    <ListItemText primary='Utilisateurs' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/paramètres">
                     <ListItemIcon>
                         <SettingsIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Paramètres' />
+                    <ListItemText primary='Paramètres' className="list-item-text" />
                 </ListItem>
 
                 <ListItem button component={Link} to="/outils">
                     <ListItemIcon>
                         <BuildIcon />
                     </ListItemIcon>
-                    <ListItemText primary='Outils' />
+                    <ListItemText primary='Outils' className="list-item-text" />
                 </ListItem>
 
                 <div className="sidebar-bottom">
-                    <FormControlLabel
-                        control={<Switch checked={darkMode} onChange={toggleDarkMode} />}
-                        label="Mode sombre"
-                        style={{ marginLeft: '10px', marginTop: '160px' }}
-                    />
+                    <ListItem>
+                        <ListItemIcon>
+                            <Brightness2Icon />
+                        </ListItemIcon>
+                        <FormControlLabel
+                            control={<Switch checked={darkMode} onChange={toggleDarkMode} />}
+                            label="Theme"
+                            // style={{ marginLeft: '10px', marginTop: '160px' }}
+                            className="list-item-text"
+                        />
+                    </ListItem>
                     <ListItem button component={Link} to="/profile">
                         <ListItemIcon>
                             <AccountCircleIcon />
                         </ListItemIcon>
-                        <ListItemText primary='Profile' />
+                        <ListItemText primary='Profile' className="list-item-text" />
                     </ListItem>
                 </div>
             </List>
