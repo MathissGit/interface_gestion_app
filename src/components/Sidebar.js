2013fed850f04b9e9b3a0ad48f2ad7f904200390
lucien-wrq <luluwerq@gmail.com>
2024-05-30 11:08:18 +0200

    j'aurais essayer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { List, ListItem, ListItemIcon, ListItemText, Switch, FormControlLabel, Slide } from '@mui/material';
+import { List, ListItem, ListItemIcon, ListItemText, Switch, FormControlLabel } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import SchoolIcon from '@mui/icons-material/School';
@@ -11,83 +11,72 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import BuildIcon from '@mui/icons-material/Build';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Brightness2Icon from '@mui/icons-material/Brightness2';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
 import { Link } from 'react-router-dom';
 
 const Sidebar = ({ toggleDarkMode, darkMode }) => {
     const [hover, setHover] = useState(false);
-    const trigger = useScrollTrigger();
 
     return (
-        <div in={!trigger}
+        <div
             className={`sidebar ${hover ? 'sidebar-expanded' : 'sidebar-collapsed'}`}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
         >
             <List className="sidebar-list">
-                <ListItem button component={Link} to="/" >
+                <ListItem button component={Link} to="/">
                     <ListItemIcon>
                         <HomeIcon />
                     </ListItemIcon>
                     <ListItemText primary='Dashboard' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/post">
                     <ListItemIcon>
                         <PostAddIcon />
                     </ListItemIcon>
                     <ListItemText primary='Post' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/cours">
                     <ListItemIcon>
                         <SchoolIcon />
                     </ListItemIcon>
                     <ListItemText primary='Cours' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/messages">
                     <ListItemIcon>
                         <MessageIcon />
                     </ListItemIcon>
                     <ListItemText primary='Messages' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/rendez-vous">
                     <ListItemIcon>
                         <CalendarTodayIcon />
                     </ListItemIcon>
                     <ListItemText primary='Entrevue' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/transaction">
                     <ListItemIcon>
                         <PaymentIcon />
                     </ListItemIcon>
                     <ListItemText primary='Transaction' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/utilisateurs">
                     <ListItemIcon>
                         <GroupIcon />
                     </ListItemIcon>
                     <ListItemText primary='Utilisateurs' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/paramètres">
                     <ListItemIcon>
                         <SettingsIcon />
                     </ListItemIcon>
                     <ListItemText primary='Paramètres' className="list-item-text" />
                 </ListItem>
-
                 <ListItem button component={Link} to="/outils">
                     <ListItemIcon>
                         <BuildIcon />
                     </ListItemIcon>
                     <ListItemText primary='Outils' className="list-item-text" />
                 </ListItem>
-
                 <div className="sidebar-bottom">
                     <ListItem>
                         <ListItemIcon>
@@ -96,7 +85,6 @@ const Sidebar = ({ toggleDarkMode, darkMode }) => {
                         <FormControlLabel
                             control={<Switch checked={darkMode} onChange={toggleDarkMode} />}
                             label="Theme"
-                            // style={{ marginLeft: '10px', marginTop: '160px' }}
                             className="list-item-text"
                         />
                     </ListItem>
